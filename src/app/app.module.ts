4bbe0acd5736d504cb8aac9530fa7b2e1ea33fa7
DimpiShah <dimpisha@gmail.com>
2019-05-17 17:13:47 +0100

    3 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import {ShoppingModule} from './shopping/shopping.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,ShoppingModule ],
+  imports:      [ BrowserModule, FormsModule,ShoppingModule,AppRoutingModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
--- src/app/shopping/shopping.module.ts
+++ src/app/shopping/shopping.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import {ShoppingListRoutingModule} from './shopping-list/shopping-list-routing.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ShoppingListRoutingModule
   ],
   declarations: [ShoppingListComponent]
 })
