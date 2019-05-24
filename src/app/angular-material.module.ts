1848388f93586fd494804bf742e94881640d0fd9
DimpiShah <dimpisha@gmail.com>
2019-05-24 22:29:54 +0100

    3 files modified

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -1,13 +1,86 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatListModule} from '@angular/material/list';
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatTableModule,
+  MatTableDataSource,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatStepperModule,
+} from '@angular/material';
+import {HttpClientModule} from '@angular/common/http';
+import {CdkTableModule} from '@angular/cdk/table';
+
+@NgModule({
+  exports: [
+    CdkTableModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+  ]
+})
 
 @NgModule({
   imports: [
     CommonModule,
-    MatListModule
+    MatSortModule
+    
   ],
   declarations: [],
-  exports:[MatListModule]
+  exports:[ MatSortModule]
 })
 export class AngularMaterialModule { }
--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {Item} from '../models/item';
+import {Unit} from '../models/unit.enum';
 import { Subject  } from 'rxjs';
 import{ItemService} from '../shared/item.service';
+import {MatPaginator, MatSort,MatTableModule, MatTableDataSource} from '@angular/material'; 
 
 @Component({
   selector: 'app-item',
@@ -10,10 +12,18 @@ import{ItemService} from '../shared/item.service';
 })
 export class ItemComponent implements OnInit {
 public items : Item[];
+public units : string[];
+ dataSource: MatTableDataSource<Item>;
+displayedColumns  = ['name', 'type', 'unit','operations'];
   constructor(private itemService:ItemService) { }
 
   ngOnInit() {
+    this.units  = Object.keys(Unit);
      this.items = this.itemService.getItems();
+     this.units = this.units.slice(this.units.length / 2);
+     this.dataSource  =  new  MatTableDataSource<Item>(this.items);
+
+     console.log(this.units);
      this.itemService.subject.subscribe({
        next:function(value)
        {
